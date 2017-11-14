0b43daef0038e307856467985155b174920fc4bd
Jeff Fitzsimmons <jeff.fitzsimmons14@gmail.com>
2017-11-14 13:59:11 -0700

    Convert to CDNs, clean up links

--- js/freelancer.js
+++ js/freelancer.js
@@ -39,17 +39,6 @@ function go(){
         maxZoom: 16,
         minZoom: 9
     }).addTo(map);
-
-    // Floating label headings for the contact form
-    // $(function() {
-    //     $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-    //         $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-    //     }).on("focus", ".floating-label-form-group", function() {
-    //         $(this).addClass("floating-label-form-group-with-focus");
-    //     }).on("blur", ".floating-label-form-group", function() {
-    //         $(this).removeClass("floating-label-form-group-with-focus");
-    //     });
-    // });
 }
 
 $( document ).ready(go());
